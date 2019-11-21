8e3b8f3d52d7a689b3853db1658a4130289c9763
VPimeshkov <56356871+VPimeshkov@users.noreply.github.com>
2019-11-21 22:11:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -48,20 +48,20 @@ class car:
     # для 1 машинки. что бы учитывать движение других машинок, нужно завести второй граф в глобали
     # и изменять его (типа прогназируемый графф)
     def calcRoute(self, graph, distance):
-        graph1 = graph
         poss = self.getPoss()
         for i in range(distance):
-            pathList = list(graph1.edges(poss))
+            pathList = list(graph.edges(poss))
             if len(pathList) == 1:
                 self.route.append(pathList[0])
                 poss = pathList[0][1]
                 # temp = (pathList[0])
             else:
                 maxMass = 0
+                labels1 = [e for e in labels]
                 for n in pathList:
-                    if int(labels[n[1]]) > maxMass:
-                        labels[n[1]] = 0
-                        maxMass = int(labels[n[1]])
+                    if int(labels1[n[1]]) > maxMass:
+                        labels1[n[1]] = 0
+                        maxMass = int(labels1[n[1]])
                         poss = n[1]
                         maxIndex = n
                 self.route.append(maxIndex)
@@ -69,31 +69,36 @@ class car:
         return self.route
 
 
-    def getRoute(self):
-        return self.route
+    def getRoute(self, graph):
+        if not self.route:
+            self.calcRoute(graph, 3)
+        route = self.route.pop(0)
+        # if len(route[0]) == 2:
+        #     self.calcRoute(graph, 1)
+        return route
 
     def setRoute(self, new_route):
         self.route = new_route
 
 
-class town_model:
-    def __init__(self, graph, labels, node_sizes, max_time=14400):
-        self.graph = graph
-        self.labels = labels
-        self.node_sizes = node_sizes
-        self.current_time = 0
-        self.max_time = max_time
-
-    def addTresh(self):
-        for n in labels.keys():
-            labels[n] = str(int(labels[n]) + delta * 5)
-        node_sizes.clear()
-        for n in nodes:
-            randTrash = int(float(labels[n]))
-            node_sizes.append(randTrash)
-            labels[n] = str(randTrash)
-        labels[0] = 0
-        node_sizes[0] = 0
+# class town_model:
+#     def __init__(self, graph, labels, node_sizes, max_time=14400):
+#         self.graph = graph
+#         self.labels = labels
+#         self.node_sizes = node_sizes
+#         self.current_time = 0
+#         self.max_time = max_time
+#
+#     def addTresh(self):
+#         for n in labels.keys():
+#             labels[n] = str(int(labels[n]) + delta * 5)
+#         node_sizes.clear()
+#         for n in nodes:
+#             randTrash = int(float(labels[n]))
+#             node_sizes.append(randTrash)
+#             labels[n] = str(randTrash)
+#         labels[0] = 0
+#         node_sizes[0] = 0
 
 
 
@@ -135,7 +140,7 @@ nodes = range(len(G.nodes))
 node_sizes = []
 labels = {}
 for n in nodes:
-    randTrash = random.randint(20, 1300)
+    randTrash = random.randint(700, 1300)
     node_sizes.append(randTrash)
     labels[n] = str(randTrash)
 labels[0] = 0
@@ -144,7 +149,7 @@ node_sizes[0] = 0
 
 def addTresh(labels, node_sizes, delta):
     for n in labels.keys():
-        labels[n] = str(int(labels[n]) + delta * 5)
+        labels[n] = str(int(labels[n]) + delta * 10)
     node_sizes.clear()
     for n in nodes:
         randTrash = int(float(labels[n]))
@@ -171,44 +176,43 @@ def wheretogo(car, G, labels):
         return (maxIndex)
 
 #
-# flag = True
-# trashMass = 0
-# for x in range(5):
-#     updateDraw(G, pos, labels, node_sizes)
-#     target = wheretogo(car1, G, labels)
-#     s = G[car1.getPoss()][target]['weight']
-#     deltatime = int(s / car1.getSpeed())
-#     print(car1.getcM())
-#     car1.changePoss(target)
-#     addTresh(labels, node_sizes, deltatime)
-#     deltatime = 0
-#     if int(labels[car1.getPoss()]) > 750:
-#         car1.addTrash(labels[car1.getPoss()])
-#         labels[car1.getPoss()] = 0
-#         deltatime = 5
-#     if car1.getcM() > car1.gettM() * 0.7:
-#         way = nx.dijkstra_path_length(G, car1.getPoss(), 0)
-#         deltatime = deltatime + (way / 1000) / car1.getSpeed()
-#         car1.changePoss(0)
-#         trashMass = trashMass + car1.getcM()
-#         car1.clearCar()
-#
-#         deltatime = deltatime + 3
-#     addTresh(labels, node_sizes, deltatime)
-#     if deltatime == 0:
-#         addTresh(labels, node_sizes, 1)
-#     print(trashMass)
-#
-#     # flag=False
+flag = True
+trashMass = 0
+car1.calcRoute(G, 5)
+for x in range(20):
+    # updateDraw(G, pos, labels, node_sizes)
+    # target = wheretogo(car1, G, labels)
+    # s = G[car1.getPoss()][target]['weight']
+    # deltatime = int(s / car1.getSpeed())
+    # print(car1.getcM())
+    # car1.changePoss(target)
+    #
 
-#
-model_time = 1440
-cars = [car1]
-for x in range(model_time):
-    for car in cars:
-        car.calcRoute(G, 3)
+    print(f"car route: {car1.route}")
     updateDraw(G, pos, labels, node_sizes)
+    target = car1.getRoute(G)
+    s = G[target[0]][target[1]]['weight']
+    deltatime = int(s / car1.getSpeed())
+    car1.changePoss(target[1])
+    print(f"current car mass: {car1.getcM()}")
 
+    addTresh(labels, node_sizes, deltatime)
+    deltatime = 0
+    if int(labels[car1.getPoss()]) > 750:
+        car1.addTrash(labels[car1.getPoss()])
+        labels[car1.getPoss()] = 0
+        deltatime = 5
+    if car1.getcM() > car1.gettM() * 0.7:
+        car1.changePoss(0)
+        car1.calcRoute(G, 5)
+        way = nx.dijkstra_path_length(G, car1.getPoss(), 0)
+        deltatime = deltatime + (way / 1000) / car1.getSpeed()
+        trashMass = trashMass + car1.getcM()
+        car1.clearCar()
 
-# test_car = car(1, 0)
-# print(test_car.calcRoute(G, 6))
+        deltatime = deltatime + 3
+    addTresh(labels, node_sizes, deltatime)
+    if deltatime == 0:
+        addTresh(labels, node_sizes, 1)
+    print(f"tresh mass: {trashMass}")
+
